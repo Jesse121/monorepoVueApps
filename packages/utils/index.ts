7c518132d5fa0c525f25898fbed3df1ab6350533
jesse <jesse152@163.com>
2022-10-16 09:38:09 +0800

    feat: 添加svgBuilder

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -3,6 +3,3 @@ export { isExternal } from "./validate";
 export * from "./cache";
 export { sync } from "./sync";
 export { default as request } from "./request";
-const module = import.meta.glob("./svgBuilder.ts", { as: "raw" });
-const svgBuilder = Object.entries(module)[1];
-export { svgBuilder };
