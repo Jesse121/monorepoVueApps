f6ce24cf5f02adbf944bc09d084a0e8e07850084
yangdong <yangdong@guotailimin.com>
2023-01-29 18:00:05 +0800

    feat: add unit demo

---
+++ packages/apps/demo/src/components/World/__test__/index.test.ts
@@ -0,0 +1,25 @@
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
+
+import World from "../index.vue";
+
+describe("World", () => {
+	test("renders a message and responds correctly to user input", () => {
+		const wrapper = shallowMount(World, {
+			props: {
+				message: "Hello World",
+				username: ""
+			}
+		});
+
+		// 确认是否渲染了 `message`
+		expect(wrapper.find(".message").text()).toEqual("Hello World");
+
+		// 断言渲染了错误信息
+		expect(wrapper.find(".error").exists()).toBeTruthy();
+
+		// // 更新 `username` 并断言错误信息不再被渲染
+		// wrapper.setData({ username: "Lachlan" });
+		// expect(wrapper.find(".error").exists()).toBeFalsy();
+	});
+});
---
+++ packages/apps/demo/src/components/World/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div class="message">
+			{{ message }}
+		</div>
+		Enter your username: <input v-model="name" />
+		<div v-if="error" class="error">Please enter a username with at least seven letters.</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+	defineProps<{
+		message: string;
+		username: string;
+	}>(),
+	{
+		message: "",
+		username: ""
+	}
+);
+
+const name = computed(() => props.username);
+
+const error = computed(() => {
+	return props.username.trim().length < 7;
+});
+</script>
